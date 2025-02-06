eeb9df12e94d70f3cab5c773a2928344b87d1de2
Andrew Tridgell <andrew@tridgell.net>
2025-02-06 15:17:39 +1100

    RC_Channel: added SYSTEMID AUX switch

--- libraries/RC_Channel/RC_Channel.h
+++ libraries/RC_Channel/RC_Channel.h
@@ -262,6 +262,8 @@ public:
         AUTOTUNE_TEST_GAINS = 180, // auto tune tuning switch to test or revert gains
         QUICKTUNE =          181,  //quicktune 3 position switch
         AHRS_AUTO_TRIM =     182,  // in-flight trim Copter AHRS using manual levelling
+        SYSTEMID =           183,  // system ID as an aux switch
+
 
         // inputs from 200 will eventually used to replace RCMAP
         ROLL =               201, // roll input
